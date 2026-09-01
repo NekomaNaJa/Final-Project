91c87522e1586f261bf89d81aa1271d37a6ae732
66160259 <66160259@go.buu.ac.th>
2026-09-01 15:28:06 +0700

    use protectedroute for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import DonatePage from "./pages/DonatePage";
-
+import ProtectedRoute from "./components/Dashboard/ProtectedRoute";
 
 const App = () => {
   return (
@@ -13,8 +13,22 @@ const App = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/donate-page" element={<DonatePage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/donate-page"
+          element={
+            <ProtectedRoute>
+              <DonatePage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
